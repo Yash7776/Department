b85df8df744945a3dc0e30a0bf7e655360b8db1f
Sachin Vishwakarma <sachinofficial71@gmail.com>
2025-06-17 12:59:44 +0530

    Unique_Header_All Added

--- app/models.py
+++ app/models.py
@@ -1,6 +1,86 @@
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+import re
+
+class UniqueIdHeaderAll(models.Model):
+    table_name = models.CharField(max_length=100)
+    id_for = models.CharField(max_length=50)
+    prefix = models.CharField(max_length=3)  # E.g., UHA, DEP
+    last_id = models.CharField(max_length=15)  # E.g., UHA-00001
+    created_on = models.DateTimeField()
+    modified_on = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.created_on:
+            self.created_on = timezone.now()
+        self.modified_on = timezone.now()
+        super().save(*args, **kwargs)
+
+    def get_next_id(self):
+        if not self.last_id:
+            # Initialize with the first ID, e.g., UHA-00001
+            next_id = f"{self.prefix}-00001"
+            self.last_id = next_id
+            self.save()
+            return next_id
+
+        # Parse the last_id, e.g., UHA-ZA001 -> prefix: UHA, alphabets: ZA, digits: 001
+        last_id_parts = self.last_id.split('-')
+        if len(last_id_parts) != 2:
+            raise ValueError(f"Invalid last_id format: {self.last_id}")
+
+        prefix, rest = last_id_parts
+        alphabets = ''.join(re.findall(r'[A-Z]', rest))
+        digits = ''.join(re.findall(r'\d+', rest))
+
+        # Total length of alphabets + digits must be 5
+        alpha_len = len(alphabets)
+        digit_len = 5 - alpha_len  # Number of digits decreases as alphabets increase
+
+        if alpha_len == 5:
+            raise ValueError("Reached the maximum ID limit: ZZZZZ")
+
+        # Check if we need to increment the alphabetic part
+        if digits == '9' * digit_len:  # e.g., 9999, 999, 99, 9
+            if alphabets == 'Z' and alpha_len == 1:
+                alphabets = 'ZA'  # Z -> ZA
+                digits = '001'    # 3 digits (ZA001)
+            elif alphabets == 'ZZ' and alpha_len == 2:
+                alphabets = 'ZZA'  # ZZ -> ZZA
+                digits = '01'      # 2 digits (ZZA01)
+            elif alphabets == 'ZZZ' and alpha_len == 3:
+                alphabets = 'ZZZZ'  # ZZZ -> ZZZZ
+                digits = '1'        # 1 digit (ZZZZ1)
+            elif alphabets == 'ZZZZ' and alpha_len == 4:
+                alphabets = 'ZZZZZ'  # ZZZZ -> ZZZZZ
+                digits = ''          # 0 digits (ZZZZZ)
+            elif alpha_len == 0:
+                alphabets = 'A'      # 9999 -> A0001
+                digits = '0001'      # 4 digits (A0001)
+            elif alpha_len in [1, 2, 3] and alphabets[-1] != 'Z':
+                # A -> B, ZA -> ZB, ZZA -> ZZB
+                last_char = alphabets[-1]
+                alphabets = alphabets[:-1] + chr(ord(last_char) + 1)
+                digits = '1'.zfill(digit_len)  # Reset digits (e.g., 0001, 001, 01)
+            elif alpha_len in [2, 3] and alphabets[-1] == 'Z':
+                # ZA -> ZZA, ZZA -> ZZZA
+                alphabets += 'A'
+                digits = '1'.zfill(digit_len - 1)  # One less digit (e.g., 001, 01)
+        else:
+            # Increment the numeric part
+            next_number = int(digits) + 1
+            digits = str(next_number).zfill(digit_len)
+
+        # Construct the next ID
+        next_id = f"{self.prefix}-{alphabets}{digits}"
+        self.last_id = next_id
+        self.save()
+        return next_id
+
+    def __str__(self):
+        return f"{self.table_name}"
+
 
 class Department(models.Model):
     dept_id = models.CharField(max_length=20, unique=True, blank=True)
